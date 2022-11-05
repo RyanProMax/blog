01bdf4f75416b7ad1fd99fd1daf3ab2d649c40bb
Ryan <627311610@qq.com>
2022-11-05 23:45:54 +0800

    chore: intro

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,26 +8,23 @@
       Ryan
     </h1>
     <p class="mt-8">
-      Hi, I'm Ryan, a fanatical front-end engineer. Working at <a href="https://www.bytedance.com/" target="__blank" class="text-red-500 text-hover text-hover-red">@ByteDance</a>.
-    </p>
-    <p class="mt-8">
-      I love coding so much, and hope to be an open-source maintainer, as good as
+      Hi, I'm Ryan, a fanatical front-end engineer who love programming so much,
+      and hope to be such a wonderful guy like
       <a href="https://antfu.me/" target="__blank" class="text-red-500 text-hover text-hover-red">@Anthony Fu</a>.
     </p>
-    <p class="mt-2">
-      But, you know, there's still a long way to go.
-      The most important thing right now is to keep learning and improve programming skills.
-    </p>
-    <p class="mt-2">
-      So, it's a website for recording something i've learned.
+    <p class="mt-8">
+      Now I work at <a
+        href="https://www.bytedance.com/" target="__blank"
+        class="text-red-500 text-hover text-hover-red"
+      >@ByteDance</a>, participate in building and optimizing the electron application with JavaScript.
     </p>
     <p class="mt-8">
-      Find more about me at <a href="https://github.com/RyanProMax/" target="__blank" class="text-red-500 text-hover text-hover-red">Github</a>.
+      Find more about me at <a
+        href="https://github.com/RyanProMax/" target="__blank"
+        class="text-red-500 text-hover text-hover-red"
+      >Github</a>.
     </p>
-    <p class="mt-8">
-      Finally, leave my favorite sentence.
-    </p>
-    <p class="mt-2 italic text-green-500">
+    <p class="mt-2 italic text-red-500">
       Stay hungry, stay foolish.
     </p>
     <footer class="w-full mt-20 text-gray-400 text-sm">
