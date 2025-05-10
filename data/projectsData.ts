6ab84ee6a726b5d2d831e4bc7adce3594b675923
Ryan <627311610@qq.com>
2025-05-11 00:02:25 +0800

    feat: tech stack

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -10,7 +10,7 @@ const projectsData: Project[] = [
     title: 'Douyin Live Studio',
     description: `Douyin's official live streaming tool, supporting various live streams including
     entertainment, gaming, and e-commerce. It offers one-click streaming start, real-time audio/video 
-    processing, beauty filters, and comment management, helping hosts create high-quality broadcasts effortlessly`,
+    processing, beauty filters, and comment management, helping hosts create high-quality broadcasts effortlessly.`,
     imgSrc: '/static/images/webcast-mate.png',
     href: 'https://streamingtool.douyin.com/',
   },
