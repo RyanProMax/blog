f1fbf6590e8a20cdbc6eec0675ec1d332a7ca0bf
Ryan <627311610@qq.com>
2025-05-27 15:48:30 +0800

    feat: projects i18n

--- app/[locale]/projects/page.tsx
+++ app/[locale]/projects/page.tsx
@@ -1,4 +1,4 @@
-import projectsData from '@/data/projectsData';
+import projectsData, { Description } from '@/data/projectsData';
 import Card from '@/components/Card';
 import { genPageMetadata } from 'app/[locale]/seo';
 import { DEFAULT_LOCALE, Locale } from '@/locales/config';
@@ -9,10 +9,7 @@ export const metadata = genPageMetadata({ title: 'Projects' });
 export default async function Page({ params }: { params: Promise<{ locale: Locale }> }) {
   const { locale = DEFAULT_LOCALE } = await params;
   const title = getNavLinkData(locale, '/projects')?.title || 'Projects';
-  const description = {
-    [Locale.EN]: `Here are some of things I've built.`,
-    [Locale.ZH]: `这是我（参与）开发的一些作品。`,
-  }[locale];
+  const desc = Description[locale];
 
   return (
     <>
@@ -21,11 +18,11 @@ export default async function Page({ params }: { params: Promise<{ locale: Local
           <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
             {title}
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">{description}</p>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">{desc}</p>
         </div>
         <div className="container py-12">
           <div className="-m-4 flex flex-wrap">
-            {projectsData.map((d) => (
+            {projectsData[locale].map((d) => (
               <Card
                 key={d.title}
                 title={d.title}
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,3 +1,5 @@
+import { Locale } from '@/locales/config';
+
 interface Project {
   title: string;
   description: string;
@@ -5,22 +7,43 @@ interface Project {
   imgSrc?: string;
 }
 
-const projectsData: Project[] = [
-  {
-    title: 'Douyin Live Studio',
-    description: `Douyin's official live streaming tool, supporting various live streams including
-    entertainment, gaming, and e-commerce. It offers one-click streaming start, real-time audio/video 
-    processing, beauty filters, and comment management, helping hosts create high-quality broadcasts effortlessly.`,
-    imgSrc: '/static/images/webcast-mate.png',
-    href: 'https://streamingtool.douyin.com/',
-  },
-  {
-    title: 'Electron React Rspack',
-    description: `An Electron-React boilerplate with TypeScript & Rspack, supporting persistent storage, 
-    local logging, incremental updates, and more.`,
-    imgSrc: '/static/images/electron-react-rspack.png',
-    href: 'https://github.com/RyanProMax/electron-react-rspack',
-  },
-];
+const projectsData: Record<Locale, Project[]> = {
+  [Locale.EN]: [
+    {
+      title: 'Douyin Live Studio',
+      description: `Support various live streams including
+      entertainment, gaming, and e-commerce. It offers one-click streaming start, real-time audio/video 
+      processing, beauty filters, and comment management, helping hosts create high-quality broadcasts effortlessly.`,
+      imgSrc: '/static/images/webcast-mate.png',
+      href: 'https://streamingtool.douyin.com/',
+    },
+    {
+      title: 'Electron React Rspack',
+      description: `An Electron-React boilerplate with TypeScript & Rspack, supporting persistent storage, 
+      local logging, incremental updates, and more.`,
+      imgSrc: '/static/images/electron-react-rspack.png',
+      href: 'https://github.com/RyanProMax/electron-react-rspack',
+    },
+  ],
+  [Locale.ZH]: [
+    {
+      title: '抖音直播伴侣',
+      description: `支持娱乐、游戏、电商等多种直播类型，提供一键开播、实时音视频处理、美颜滤镜和评论管理等功能，助力主播高效打造高质量直播内容。`,
+      imgSrc: '/static/images/webcast-mate.png',
+      href: 'https://streamingtool.douyin.com/',
+    },
+    {
+      title: 'Electron React Rspack',
+      description: `基于 TypeScript 和 Rspack 的 Electron-React 脚手架模板，支持持久化存储、本地日志记录、增量更新等功能，适用于构建跨平台桌面应用。`,
+      imgSrc: '/static/images/electron-react-rspack.png',
+      href: 'https://github.com/RyanProMax/electron-react-rspack',
+    },
+  ],
+};
 
 export default projectsData;
+
+export const Description = {
+  [Locale.EN]: `Here are some of things I've built.`,
+  [Locale.ZH]: `这是我（参与）开发的一些作品。`,
+};
